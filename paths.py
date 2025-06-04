c29e01ef0f498d97351690617879439309577add
RayStormThunder <Raystormthunder@gmail.com>
2025-06-04 11:14:03 -0400

    Correct Paths v3

--- paths.py
+++ paths.py
@@ -14,10 +14,9 @@ except ImportError:
     RANDO_ROOT_PATH = Path(os.path.dirname(os.path.realpath(__file__)))
     IS_RUNNING_FROM_SOURCE = True
 
-# Get the directory of the executable or script
 if getattr(sys, "frozen", False):
-    RANDO_EXE_ROOT_PATH = (
-        Path(sys._MEIPASS) if hasattr(sys, "_MEIPASS") else Path(sys.executable).parent
-    )
+    # Always use the folder the .exe is in
+    RANDO_EXE_ROOT_PATH = Path(sys.executable).parent
 else:
-    RANDO_EXE_ROOT_PATH = Path(__file__).parent
+    # Use the folder the script is in
+    RANDO_EXE_ROOT_PATH = Path(__file__).parent.resolve()
